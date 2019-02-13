1df3a89c05b189e30539286920e28310091744ad
alexjunior012 <alexjunior.web@gmail.com>
2019-02-13 15:18:13 -0200

    links das redes sociais

---
+++ src/routes/SocialNetworks.js
@@ -0,0 +1,30 @@
+import { faFacebookF, faInstagram, faYoutube, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+
+const social = [
+    {
+        name: 'Facebook',
+        url: 'https://www.facebook.com/saogregoriofc',
+        title: 'Curta nossa página no Facebook',
+        icon: faFacebookF,
+    },
+    {
+        name: 'Instagram',
+        url: 'https://www.instagram.com/saogregoriofc',
+        title: 'Siga-nos no instagram',
+        icon: faInstagram,
+    },
+    {
+        name: 'Youtube',
+        url: '',
+        title: 'Assine nosso canal no youtube',
+        icon: faYoutube,
+    },
+    {
+        name: 'Whattsapp',
+        url: '',
+        title: 'Entre em contato conosco via whattsapp',
+        icon: faWhatsapp,
+    },
+];
+
+export default social;
